729087c689c0a6d4c45b0aa3d0d6f7071bff2eda
Ayman Mahfouz <amahfouz@webalo.com>
2018-12-16 16:21:47 -0800

    Added 2017.

---
+++ src/mahfouz/google/codejam/y2017/round1b/ASmall.java
@@ -0,0 +1,25 @@
+package mahfouz.google.codejam.y2017.round1b;
+
+import java.util.Scanner;
+
+public final class ASmall {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int numCases = s.nextInt();
+        s.nextLine();
+
+        for (int i = 0; i < numCases; i++) {
+            solveCase(i+1, s);
+        }
+    }
+
+    private static void solveCase(int caseNum, Scanner s) {
+        int x = s.nextInt();
+
+
+        int solution = 0;
+        System.out.println("Case #" + caseNum + ": " + solution);
+    }
+
+}
